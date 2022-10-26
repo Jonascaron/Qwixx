6c61298bab031b243b0c0c443b7772feb41c738c
Jonas Caron <jonas.caron16@gmail.com>
2022-10-26 11:33:34 +0200

    Add project files.

---
+++ qwixx/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace qwixx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRood = new System.Windows.Forms.TextBox();
+            this.txtGeel = new System.Windows.Forms.TextBox();
+            this.txtGeel2 = new System.Windows.Forms.TextBox();
+            this.txtGroen = new System.Windows.Forms.TextBox();
+            this.txtBlauw = new System.Windows.Forms.TextBox();
+            this.txtRood2 = new System.Windows.Forms.TextBox();
+            this.txtBlauw2 = new System.Windows.Forms.TextBox();
+            this.txtGroen2 = new System.Windows.Forms.TextBox();
+            this.btnGooi = new System.Windows.Forms.Button();
+            this.txtResultaat1 = new System.Windows.Forms.TextBox();
+            this.txtResultaat2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtRood
+            // 
+            this.txtRood.BackColor = System.Drawing.Color.Red;
+            this.txtRood.Location = new System.Drawing.Point(12, 310);
+            this.txtRood.Name = "txtRood";
+            this.txtRood.Size = new System.Drawing.Size(100, 22);
+            this.txtRood.TabIndex = 145;
+            // 
+            // txtGeel
+            // 
+            this.txtGeel.BackColor = System.Drawing.Color.Yellow;
+            this.txtGeel.Location = new System.Drawing.Point(118, 310);
+            this.txtGeel.Name = "txtGeel";
+            this.txtGeel.Size = new System.Drawing.Size(100, 22);
+            this.txtGeel.TabIndex = 146;
+            // 
+            // txtGeel2
+            // 
+            this.txtGeel2.BackColor = System.Drawing.Color.Yellow;
+            this.txtGeel2.Location = new System.Drawing.Point(1201, 310);
+            this.txtGeel2.Name = "txtGeel2";
+            this.txtGeel2.Size = new System.Drawing.Size(100, 22);
+            this.txtGeel2.TabIndex = 147;
+            // 
+            // txtGroen
+            // 
+            this.txtGroen.BackColor = System.Drawing.Color.Lime;
+            this.txtGroen.Location = new System.Drawing.Point(224, 310);
+            this.txtGroen.Name = "txtGroen";
+            this.txtGroen.Size = new System.Drawing.Size(100, 22);
+            this.txtGroen.TabIndex = 148;
+            // 
+            // txtBlauw
+            // 
+            this.txtBlauw.BackColor = System.Drawing.Color.Blue;
+            this.txtBlauw.Location = new System.Drawing.Point(330, 310);
+            this.txtBlauw.Name = "txtBlauw";
+            this.txtBlauw.Size = new System.Drawing.Size(100, 22);
+            this.txtBlauw.TabIndex = 149;
+            // 
+            // txtRood2
+            // 
+            this.txtRood2.BackColor = System.Drawing.Color.Red;
+            this.txtRood2.Location = new System.Drawing.Point(1095, 310);
+            this.txtRood2.Name = "txtRood2";
+            this.txtRood2.Size = new System.Drawing.Size(100, 22);
+            this.txtRood2.TabIndex = 150;
+            // 
+            // txtBlauw2
+            // 
+            this.txtBlauw2.BackColor = System.Drawing.Color.Blue;
+            this.txtBlauw2.Location = new System.Drawing.Point(1413, 310);
+            this.txtBlauw2.Name = "txtBlauw2";
+            this.txtBlauw2.Size = new System.Drawing.Size(100, 22);
+            this.txtBlauw2.TabIndex = 151;
+            // 
+            // txtGroen2
+            // 
+            this.txtGroen2.BackColor = System.Drawing.Color.Lime;
+            this.txtGroen2.Location = new System.Drawing.Point(1307, 310);
+            this.txtGroen2.Name = "txtGroen2";
+            this.txtGroen2.Size = new System.Drawing.Size(100, 22);
+            this.txtGroen2.TabIndex = 152;
+            // 
+            // btnGooi
+            // 
+            this.btnGooi.Location = new System.Drawing.Point(519, 367);
+            this.btnGooi.Name = "btnGooi";
+            this.btnGooi.Size = new System.Drawing.Size(75, 34);
+            this.btnGooi.TabIndex = 153;
+            this.btnGooi.Text = "goien";
+            this.btnGooi.UseVisualStyleBackColor = true;
+            this.btnGooi.Click += new System.EventHandler(this.btnGooi_Click);
+            // 
+            // txtResultaat1
+            // 
+            this.txtResultaat1.Location = new System.Drawing.Point(436, 310);
+            this.txtResultaat1.Name = "txtResultaat1";
+            this.txtResultaat1.Size = new System.Drawing.Size(100, 22);
+            this.txtResultaat1.TabIndex = 154;
+            // 
+            // txtResultaat2
+            // 
+            this.txtResultaat2.Location = new System.Drawing.Point(1519, 310);
+            this.txtResultaat2.Name = "txtResultaat2";
+            this.txtResultaat2.Size = new System.Drawing.Size(100, 22);
+            this.txtResultaat2.TabIndex = 155;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1671, 736);
+            this.Controls.Add(this.txtResultaat2);
+            this.Controls.Add(this.txtResultaat1);
+            this.Controls.Add(this.btnGooi);
+            this.Controls.Add(this.txtGroen2);
+            this.Controls.Add(this.txtBlauw2);
+            this.Controls.Add(this.txtRood2);
+            this.Controls.Add(this.txtBlauw);
+            this.Controls.Add(this.txtGroen);
+            this.Controls.Add(this.txtGeel2);
+            this.Controls.Add(this.txtGeel);
+            this.Controls.Add(this.txtRood);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Qwixx";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtRood;
+        private System.Windows.Forms.TextBox txtGeel;
+        private System.Windows.Forms.TextBox txtGeel2;
+        private System.Windows.Forms.TextBox txtGroen;
+        private System.Windows.Forms.TextBox txtBlauw;
+        private System.Windows.Forms.TextBox txtRood2;
+        private System.Windows.Forms.TextBox txtBlauw2;
+        private System.Windows.Forms.TextBox txtGroen2;
+        private System.Windows.Forms.Button btnGooi;
+        private System.Windows.Forms.TextBox txtResultaat1;
+        private System.Windows.Forms.TextBox txtResultaat2;
+    }
+}
+
---
+++ qwixx/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace qwixx
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            WindowState = FormWindowState.Maximized;
+        }
+        Button[] arrayRijRood = new Button[12];
+        Button[] arrayRijGeel = new Button[12];
+        Button[] arrayRijGroen = new Button[12];
+        Button[] arrayRijBlauw = new Button[12];
+        Button[] arrayRijRood2 = new Button[12];
+        Button[] arrayRijGeel2 = new Button[12];
+        Button[] arrayRijGroen2 = new Button[12];
+        Button[] arrayRijBlauw2 = new Button[12];
+        int intHoeVer;
+        bool turn = false;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kaartOpScherm(arrayRijRood, 10, 50, Color.Red, 2, 2);
+            kaartOpScherm(arrayRijGeel, 10, 100, Color.Yellow, 2, 2);
+            kaartOpScherm(arrayRijGroen, 10, 150, Color.Green, 12, 12);
+            kaartOpScherm(arrayRijBlauw, 10, 200, Color.Blue, 12, 12);
+            kaartOpScherm(arrayRijRood2, 850, 50, Color.Red, 2, 2);
+            kaartOpScherm(arrayRijGeel2, 850, 100, Color.Yellow, 2, 2);
+            kaartOpScherm(arrayRijGroen2, 850, 150, Color.Green, 12, 12);
+            kaartOpScherm(arrayRijBlauw2, 850, 200, Color.Blue, 12, 12);
+            disableButtons(arrayRijRood2, Color.Red);
+            disableButtons(arrayRijGeel2, Color.Yellow);
+            disableButtons(arrayRijGroen2, Color.Green);
+            disableButtons(arrayRijBlauw2, Color.Blue);
+            disableButtons(arrayRijRood, Color.Red);
+            disableButtons(arrayRijGeel, Color.Yellow);
+            disableButtons(arrayRijGroen, Color.Green);
+            disableButtons(arrayRijBlauw, Color.Blue);
+        }
+
+        public void kaartOpScherm(Button[] arraybutton, int xPos, int ypos, Color color, int a, int b)
+        {
+            for (int i = 1; i < arraybutton.Length; i++)
+            {
+                arraybutton[i] = new Button();
+                arraybutton[i].BackColor = color;
+                arraybutton[i].TextAlign = ContentAlignment.MiddleCenter;
+                arraybutton[i].Text = a.ToString();
+                arraybutton[i].Tag = i;
+                arraybutton[i].Font = new Font("Serif", 18, FontStyle.Bold);
+                arraybutton[i].Size = new Size(50, 50);
+                arraybutton[i].Location = new Point(xPos + (i % 12) * 50, ypos + (i / 12) * 60);
+                this.Controls.Add(arraybutton[i]);
+                if (b == 2)
+                {
+                    a++;
+                }
+                else
+                {
+                    a--;
+                }
+                arraybutton[i].Click += ButtonOnClick;
+            }
+
+
+        }
+
+        public void ButtonOnClick(object sender, EventArgs e)
+        {
+            Button vakje = (Button)sender;
+            Button vakje2 = (Button)sender;
+            int tellerRood = 0;
+            int tellerGeel = 0;
+            int tellerGroen = 0;
+            int tellerBlauw = 0;
+            int tellerRood2 = 0;
+            int tellerGeel2 = 0;
+            int tellerGroen2 = 0;
+            int tellerBlauw2 = 0;
+            int getalenVak1, getalenVak2;
+            int getalRood = 0;
+            int getalGeel = 0;
+            int getalGroen = 0;
+            int getalBlauw = 0;
+            int getalRood2 = 0;
+            int getalGeel2 = 0;
+            int getalGroen2 = 0;
+            int getalBlauw2 = 0;
+            intHoeVer = int.Parse(vakje.Tag.ToString());
+            if (turn == true)
+            {
+                VeranderKleur(arrayRijRood, Color.Red, vakje, Color.LightPink);
+                VeranderKleur(arrayRijGeel, Color.Yellow, vakje, Color.LightYellow);
+                VeranderKleur(arrayRijGroen, Color.Green, vakje, Color.LightGreen);
+                VeranderKleur(arrayRijBlauw, Color.Blue, vakje, Color.LightBlue);
+            }
+            else
+            {
+                VeranderKleur(arrayRijRood2, Color.Red, vakje2, Color.LightPink);
+                VeranderKleur(arrayRijGeel2, Color.Yellow, vakje2, Color.LightYellow);
+                VeranderKleur(arrayRijGroen2, Color.Green, vakje2, Color.LightGreen);
+                VeranderKleur(arrayRijBlauw2, Color.Blue, vakje2, Color.LightBlue);
+            }
+            VakjeTellen(tellerRood, arrayRijRood, Color.LightPink, txtRood);
+            VakjeTellen(tellerGeel, arrayRijGeel, Color.LightYellow, txtGeel);
+            VakjeTellen(tellerGroen, arrayRijGroen, Color.LightGreen, txtGroen);
+            VakjeTellen(tellerBlauw, arrayRijBlauw, Color.LightBlue, txtBlauw);
+
+            VakjeTellen(tellerRood2, arrayRijRood2, Color.LightPink, txtRood2);
+            VakjeTellen(tellerGeel2, arrayRijGeel2, Color.LightYellow, txtGeel2);
+            VakjeTellen(tellerGroen2, arrayRijGroen2, Color.LightGreen, txtGroen2);
+            VakjeTellen(tellerBlauw2, arrayRijBlauw2, Color.LightBlue, txtBlauw2);
+
+            try { 
+                getalRood = int.Parse(txtRood.Text);
+            } catch { }
+            try
+            {
+                getalGeel = int.Parse(txtGeel.Text);
+            } catch { }
+            try
+            {
+                getalGroen = int.Parse(txtGroen.Text);
+            } catch { }
+            try
+            {
+                getalBlauw = int.Parse(txtBlauw.Text);
+            } catch { }
+            try
+            {
+                getalRood2 = int.Parse(txtRood2.Text);
+            } catch { }
+            try
+            {
+                getalGeel2 = int.Parse(txtGeel2.Text);
+            } catch { }
+            try
+            {
+                getalGroen2 = int.Parse(txtGroen2.Text);
+            } catch { }
+            try
+            {
+                getalBlauw2 = int.Parse(txtBlauw2.Text);
+            } catch { }
+
+            getalenVak1 = getalRood + getalGeel + getalGroen + getalBlauw;
+            txtResultaat1.Text = getalenVak1.ToString();
+            getalenVak2 = getalRood2 + getalGeel2 + getalGroen2 + getalBlauw2;
+            txtResultaat2.Text = getalenVak2.ToString();
+
+        }
+
+        public void VakjeTellen(int teller, Button[] arrayButton, Color kleur, TextBox text)
+        {
+            for (int i = 1; i < arrayButton.Length; i++)
+            {
+                if (arrayButton[i].BackColor == kleur)
+                {
+                    teller++;
+                    switch (teller)
+                    {
+                        case 1:
+                            text.Text = 1.ToString();
+                            break;
+                        case 2:
+                            text.Text = 3.ToString();
+                            break;
+                        case 3:
+                            text.Text = 6.ToString();
+                            break;
+                        case 4:
+                            text.Text = 10.ToString();
+                            break;
+                        case 5:
+                            text.Text = 15.ToString();
+                            break;
+                        case 6:
+                            text.Text = 21.ToString();
+                            break;
+                        case 7:
+                            text.Text = 28.ToString();
+                            break;
+                        case 8:
+                            text.Text = 36.ToString();
+                            break;
+                        case 9:
+                            text.Text = 45.ToString();
+                            break;
+                        case 10:
+                            text.Text = 55.ToString();
+                            break;
+                        case 11:
+                            text.Text = 66.ToString();
+                            break;
+                        case 12:
+                            text.Text = 78.ToString();
+                            break;
+                    }
+                }
+            }
+        }
+        public void VeranderKleur(Button[] arrayButton, Color kleur, Button vakje, Color kleur2)
+        {
+
+
+            if (vakje.BackColor == kleur)
+            {
+                for (int i = 1; i <= intHoeVer; i++)
+                {
+                    if (arrayButton[i].BackColor == kleur)
+                    {
+                        arrayButton[i].Enabled = false;
+                        arrayButton[i].BackColor = TransparencyKey;
+                    }
+
+                }
+                vakje.BackColor = kleur2;
+
+            }
+        }
+
+        public void disableButtons(Button[] arrayButton, Color kleur)
+        {
+            for (int i = 1; i < arrayButton.Length; i++)
+            {
+                if (arrayButton[i].BackColor == kleur)
+                {
+                    arrayButton[i].Enabled = false;
+                }
+            }
+        }
+        public void enableButtons(Button[] arrayButton, Color Kleur)
+        {
+            for (int i = 1; i < arrayButton.Length; i++)
+            {
+                if (arrayButton[i].BackColor == Kleur)
+                {
+                    arrayButton[i].Enabled = true;
+                }
+            }
+        }
+
+        private void btnGooi_Click(object sender, EventArgs e)
+        {
+            turn = !turn;
+            if (turn == true)
+            {
+                disableButtons(arrayRijRood2, Color.Red);
+                disableButtons(arrayRijGeel2, Color.Yellow);
+                disableButtons(arrayRijGroen2, Color.Green);
+                disableButtons(arrayRijBlauw2, Color.Blue);
+                enableButtons(arrayRijRood, Color.Red);
+                enableButtons(arrayRijGeel, Color.Yellow);
+                enableButtons(arrayRijGroen, Color.Green);
+                enableButtons(arrayRijBlauw, Color.Blue);
+            }
+            else
+            {
+                disableButtons(arrayRijRood, Color.Red);
+                disableButtons(arrayRijGeel, Color.Yellow);
+                disableButtons(arrayRijGroen, Color.Green);
+                disableButtons(arrayRijBlauw, Color.Blue);
+                enableButtons(arrayRijRood2, Color.Red);
+                enableButtons(arrayRijGeel2, Color.Yellow);
+                enableButtons(arrayRijGroen2, Color.Green);
+                enableButtons(arrayRijBlauw2, Color.Blue);
+            }
+        }
+    }
+}
